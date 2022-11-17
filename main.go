ff56cd35fcd47ce57d306da4e2c8393b9c85a707
Jayadeep KM <kmjayadeep@gmail.com>
2022-11-17 18:34:15 +0100

    add reconciler for ingress
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- main.go
+++ main.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	networkingv1 "k8s.io/api/networking/v1"
+
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+func main() {
+	logf.SetLogger(zap.New())
+
+	var log = logf.Log.WithName("builder-examples")
+
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	if err != nil {
+		log.Error(err, "could not create manager")
+		os.Exit(1)
+	}
+
+	err = builder.
+		ControllerManagedBy(mgr).
+		For(&networkingv1.Ingress{}).
+		Complete(&IngressDnsController{})
+	if err != nil {
+		log.Error(err, "could not create controller")
+		os.Exit(1)
+	}
+
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		log.Error(err, "could not start manager")
+		os.Exit(1)
+	}
+}
+
+// IngressDnsController is a simple ControllerManagedBy example implementation.
+type IngressDnsController struct {
+	client.Client
+}
+
+func (a *IngressDnsController) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+	ing := &networkingv1.Ingress{}
+	err := a.Get(ctx, req.NamespacedName, ing)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	for _, rule := range ing.Spec.Rules {
+		host := rule.Host
+		if !strings.HasSuffix(host, "duckdns.org") {
+			continue
+		}
+		logf.Log.Info("got duckdns domain", "host", host)
+	}
+
+	return reconcile.Result{}, nil
+}
+
+func (a *IngressDnsController) InjectClient(c client.Client) error {
+	a.Client = c
+	return nil
+}
